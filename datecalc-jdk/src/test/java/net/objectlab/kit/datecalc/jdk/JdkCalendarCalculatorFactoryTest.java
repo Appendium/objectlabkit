d8c2a7f248a9aa99e399ac6d3774c9f1878ccfd4
benoitx <benoitx@02d1aa78-701b-0410-9600-97ca906d73b6>
2006-09-07 17:34:27 +0000

    Found a "nice" way to include the common classes as a JAR in the JDK and JODA jars.

---
+++ datecalc-jdk/src/test/java/net/objectlab/kit/datecalc/jdk/JdkCalendarCalculatorFactoryTest.java
@@ -0,0 +1,20 @@
+package net.objectlab.kit.datecalc.jdk;
+
+import java.util.Calendar;
+
+import net.objectlab.kit.datecalc.common.AbstractDateCalculatorFactoryTest;
+import net.objectlab.kit.datecalc.common.DateCalculatorFactory;
+import net.objectlab.kit.datecalc.common.Utils;
+
+public class JdkCalendarCalculatorFactoryTest extends AbstractDateCalculatorFactoryTest<Calendar> {
+
+    @Override
+    protected Calendar newDate(final String date) {
+        return Utils.createCalendar(date);
+    }
+
+    @Override
+    protected DateCalculatorFactory<Calendar> getDateCalculatorFactory() {
+        return DefaultJdkCalendarCalculatorFactory.getDefaultInstance();
+    }
+}
