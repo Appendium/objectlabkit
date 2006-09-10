59a6a96e6e5b9942d932acba5116840443763f8e
Marcin Jekot <naartjie@users.noreply.github.com>
2006-09-10 14:51:14 +0000

    updated licence comment on .java files

--- datecalc-jdk/src/main/java/net/objectlab/kit/datecalc/jdk/DateModifiedPreceedingHandler.java
+++ datecalc-jdk/src/main/java/net/objectlab/kit/datecalc/jdk/DateModifiedPreceedingHandler.java
@@ -14,7 +14,8 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- */package net.objectlab.kit.datecalc.jdk;
+ */
+package net.objectlab.kit.datecalc.jdk;
 
 import java.util.Date;
 
