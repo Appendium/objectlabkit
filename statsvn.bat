f07a197ac3b687b9e144825fed36fb93afa8224f
Benoit Xhenseval <benoitx@users.noreply.github.com>
2006-11-21 10:25:32 +0000

    Build stats.

--- statsvn.bat
+++ statsvn.bat
@@ -1,3 +1,3 @@
 svn log -v --xml > logfile.log
 rem mkdir qalab\target\docs\statsvn
-java -jar c:\java\statsvn\statsvn.jar -css objectlab-statcvs.css -verbose -output-dir src\site\statsvn -title ObjectLabKit -viewvc http://svn.sourceforge.net/viewvc/objectlabkit/trunk ./logfile.log .
+java -jar c:\java\statsvn\statsvn.jar -format xdoc -verbose -output-dir src\site\statsvn -title ObjectLabKit -viewvc http://svn.sourceforge.net/viewvc/objectlabkit/trunk ./logfile.log .
