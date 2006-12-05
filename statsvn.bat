3ea50959f9e7f677c36950a1216f7f562390500d
benoitx <benoitx@02d1aa78-701b-0410-9600-97ca906d73b6>
2006-12-05 00:46:20 +0000

    Improved to use the new tags option (coming soon in a StatSVN close to you).

--- statsvn.bat
+++ statsvn.bat
@@ -1,3 +1,3 @@
-svn log -v --xml > logfile.log
+svn log -v --xml https://svn.sourceforge.net/svnroot/objectlabkit/ > logfile.log
 rem mkdir qalab\target\docs\statsvn
-java -jar c:\java\statsvn\statsvn.jar -format xdoc -verbose -output-dir src\site\statsvn -title ObjectLabKit -viewvc http://svn.sourceforge.net/viewvc/objectlabkit/trunk ./logfile.log .
+java -jar c:\java\statsvn\statsvn.jar -format xdoc -verbose -output-dir src\site\statsvn -tags "^1.0.1" -title ObjectLabKit -viewvc http://svn.sourceforge.net/viewvc/objectlabkit/trunk ./logfile.log .
