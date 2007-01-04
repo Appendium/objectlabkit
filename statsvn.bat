9c75fee2f6a7f59dfb4d7a10f41cfec182faf439
benoitx <benoitx@02d1aa78-701b-0410-9600-97ca906d73b6>
2007-01-04 00:33:01 +0000

    fixed script following upgrade to StatSVN 0.3.x

--- statsvn.bat
+++ statsvn.bat
@@ -1,3 +1,3 @@
-svn log -v --xml https://svn.sourceforge.net/svnroot/objectlabkit/ > logfile.log
-rem mkdir qalab\target\docs\statsvn
-java -jar c:\java\statsvn\statsvn.jar -xdoc -verbose -output-dir src\site\statsvn -tags "^1.0.1|^1.1.0" -title ObjectLabKit -exclude "**/qalab.xml" -viewvc http://svn.sourceforge.net/viewvc/objectlabkit/trunk ./logfile.log .
+svn log -v --xml https://svn.sourceforge.net/svnroot/objectlabkit/ > svn.log
+rem mkdir src\site\statsvn
+java -jar c:\java\statsvn\statsvn.jar -xdoc -verbose -output-dir src\site\statsvn -tags "^1.0.1|^1.1.0" -title ObjectLabKit -exclude "**/qalab.xml" -viewvc http://svn.sourceforge.net/viewvc/objectlabkit/trunk ./svn.log .
