3777ac131460a784cf053614448b8ac5ba4faf10
benoitx <benoitx@02d1aa78-701b-0410-9600-97ca906d73b6>
2007-01-25 10:37:59 +0000

    relocate

--- statsvn.bat
+++ statsvn.bat
@@ -1,3 +1,3 @@
-svn log -v --xml https://svn.sourceforge.net/svnroot/objectlabkit/ > svn.log
+svn log -v --xml https://objectlabkit.svn.sourceforge.net/svnroot/objectlabkit/ > svn.log
 rem mkdir src\site\statsvn
 java -jar c:\java\statsvn\statsvn.jar -xdoc -verbose -output-dir src\site\statsvn -tags "^1.0.1|^1.1.0" -title ObjectLabKit -exclude "**/qalab.xml" -viewvc http://svn.sourceforge.net/viewvc/objectlabkit/trunk ./svn.log .
