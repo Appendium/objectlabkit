86330e0de78ebed4377f1ec4c66e189b826db4c0
Benoit Xhenseval <benoitx@users.noreply.github.com>
2010-03-26 21:24:51 +0000

    Tidy up and adding a skeleton for test.

---
+++ utils/src/test/java/net/objectlab/kit/util/BigDecimalUtilTest.java
@@ -0,0 +1,491 @@
+/**
+ * 
+ */
+package net.objectlab.kit.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+/**
+ * @author Benoit
+ *
+ */
+public class BigDecimalUtilTest {
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#inverse(java.math.BigDecimal, int)}.
+     */
+    @Test
+    public void testInverseBigDecimalInt() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#inverse(java.math.BigDecimal)}.
+     */
+    @Test
+    public void testInverseBigDecimal() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#isNotZero(java.math.BigDecimal)}.
+     */
+    @Test
+    public void testIsNotZero() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#isZero(java.math.BigDecimal)}.
+     */
+    @Test
+    public void testIsZero() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#isNegative(java.math.BigDecimal)}.
+     */
+    @Test
+    public void testIsNegative() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#isStrictlyPositive(java.math.BigDecimal)}.
+     */
+    @Test
+    public void testIsStrictlyPositive() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#isNullOrZero(java.math.BigDecimal)}.
+     */
+    @Test
+    public void testIsNullOrZero() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#isSameValue(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testIsSameValue() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#isSameValueTreatNullAsZero(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testIsSameValueTreatNullAsZero() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#add(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testAddBigDecimalBigDecimal() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#add(java.math.BigDecimal, java.math.BigDecimal[])}.
+     */
+    @Test
+    public void testAddBigDecimalBigDecimalArray() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#subtract(java.math.BigDecimal, java.math.BigDecimal[])}.
+     */
+    @Test
+    public void testSubtractBigDecimalBigDecimalArray() {
+        assertEquals("2 null", BigDecimal.ZERO, BigDecimalUtil.subtract(null, (BigDecimal[]) null));
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#subtract(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testSubtractBigDecimalBigDecimal() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#divide(java.math.BigDecimal, java.math.BigDecimal, int)}.
+     */
+    @Test
+    public void testDivideBigDecimalBigDecimalInt() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#calculateWeight(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testCalculateWeight() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#divide(int, java.math.BigDecimal, java.math.BigDecimal, int)}.
+     */
+    @Test
+    public void testDivideIntBigDecimalBigDecimalInt() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#divide(java.math.BigDecimal, java.math.BigDecimal, int, int)}.
+     */
+    @Test
+    public void testDivideBigDecimalBigDecimalIntInt() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#multiply(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testMultiplyBigDecimalBigDecimal() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#multiply(java.math.BigDecimal, java.math.BigDecimal[])}.
+     */
+    @Test
+    public void testMultiplyBigDecimalBigDecimalArray() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#abs(java.math.BigDecimal)}.
+     */
+    @Test
+    public void testAbs() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#negate(java.math.BigDecimal)}.
+     */
+    @Test
+    public void testNegate() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#negateIfTrue(boolean, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testNegateIfTrue() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#isNotSameAbsValue(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testIsNotSameAbsValue() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#isNotSameValue(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testIsNotSameValue() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#isSameAbsValue(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testIsSameAbsValue() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#compareTo(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testCompareTo() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#absCompareTo(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testAbsCompareTo() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#absDiff(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testAbsDiff() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#movePoint(java.math.BigDecimal, int)}.
+     */
+    @Test
+    public void testMovePoint() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#roundTo(java.math.BigDecimal, int, int)}.
+     */
+    @Test
+    public void testRoundTo() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#setScale(java.math.BigDecimal, int)}.
+     */
+    @Test
+    public void testSetScaleBigDecimalInt() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#setScale(java.math.BigDecimal, java.lang.Integer)}.
+     */
+    @Test
+    public void testSetScaleBigDecimalInteger() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#setScale(java.math.BigDecimal, java.lang.Integer, int)}.
+     */
+    @Test
+    public void testSetScaleBigDecimalIntegerInt() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#signum(java.math.BigDecimal)}.
+     */
+    @Test
+    public void testSignum() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#isSameSignum(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testIsSameSignum() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#hasSignedFlippedAndNotZero(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testHasSignedFlippedAndNotZero() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#hasSignedChanged(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testHasSignedChanged() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#isOutsideRange(java.math.BigDecimal, java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testIsOutsideRange() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#isInsideInclusiveRange(java.math.BigDecimal, java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testIsInsideInclusiveRange() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#assignNonNull(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testAssignNonNull() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#addWeightedConstituent(java.math.BigDecimal, java.math.BigDecimal, java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testAddWeightedConstituent() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#allNullOrZero(java.math.BigDecimal[])}.
+     */
+    @Test
+    public void testAllNullOrZero() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#format(java.math.BigDecimal)}.
+     */
+    @Test
+    public void testFormat() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#percentFormat(java.math.BigDecimal)}.
+     */
+    @Test
+    public void testPercentFormat() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#movedInsideThresholdPercentage(java.math.BigDecimal, java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testMovedInsideThresholdPercentage() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#movedStrictlyOutsideThresholdPercentage(java.math.BigDecimal, java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testMovedStrictlyOutsideThresholdPercentage() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#roundUp(java.math.BigDecimal, int)}.
+     */
+    @Test
+    public void testRoundUp() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#roundDown(java.math.BigDecimal, int)}.
+     */
+    @Test
+    public void testRoundDown() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#roundUpForIncrement(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testRoundUpForIncrement() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#roundDownForIncrement(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testRoundDownForIncrement() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#ensureMin(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testEnsureMin() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#forceNegative(java.math.BigDecimal)}.
+     */
+    @Test
+    public void testForceNegative() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#forceNegativeIfTrue(boolean, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testForceNegativeIfTrue() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#min(java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testMin() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#max(java.math.BigDecimal[])}.
+     */
+    @Test
+    public void testMax() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#longForFraction(java.math.BigDecimal)}.
+     */
+    @Test
+    public void testLongForFraction() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#isDiffMoreThanAbsThreshold(java.math.BigDecimal, java.math.BigDecimal, java.math.BigDecimal)}.
+     */
+    @Test
+    public void testIsDiffMoreThanAbsThreshold() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#doubleValue(java.math.BigDecimal)}.
+     */
+    @Test
+    public void testDoubleValue() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.objectlab.kit.util.BigDecimalUtil#isZeroOrLess(java.math.BigDecimal)}.
+     */
+    @Test
+    public void testIsZeroOrLess() {
+        fail("Not yet implemented");
+    }
+
+}
