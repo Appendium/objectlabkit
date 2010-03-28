43311715274145478818d9761d0c192eb847a83b
benoitx <benoitx@02d1aa78-701b-0410-9600-97ca906d73b6>
2010-03-28 11:46:42 +0000

    100% coverage for BooleanUtil

--- utils/src/test/java/net/objectlab/kit/util/BooleanUtilTest.java
+++ utils/src/test/java/net/objectlab/kit/util/BooleanUtilTest.java
@@ -8,27 +8,51 @@ public class BooleanUtilTest {
 
     @Test
     public void testIsTrueOrNull() {
-        fail("Not yet implemented");
+        assertTrue("null", BooleanUtil.isTrueOrNull(null));
+        assertTrue("Boolean.TRUE", BooleanUtil.isTrueOrNull(Boolean.TRUE));
+        assertTrue("true", BooleanUtil.isTrueOrNull(true));
+        assertFalse("Boolean.FALSE", BooleanUtil.isTrueOrNull(Boolean.FALSE));
+        assertFalse("false", BooleanUtil.isTrueOrNull(false));
     }
 
     @Test
     public void testIsFalseOrNull() {
-        fail("Not yet implemented");
+        assertTrue("null", BooleanUtil.isFalseOrNull(null));
+        assertFalse("Boolean.TRUE", BooleanUtil.isFalseOrNull(Boolean.TRUE));
+        assertFalse("true", BooleanUtil.isFalseOrNull(true));
+        assertTrue("Boolean.FALSE", BooleanUtil.isFalseOrNull(Boolean.FALSE));
+        assertTrue("false", BooleanUtil.isFalseOrNull(false));
     }
 
     @Test
     public void testIsTrueBoolean() {
-        fail("Not yet implemented");
+        assertFalse("null", BooleanUtil.isTrue((Boolean)null));
+        assertTrue("Boolean.TRUE", BooleanUtil.isTrue(Boolean.TRUE));
+        assertFalse("Boolean.FALSE", BooleanUtil.isTrue(Boolean.FALSE));
     }
 
     @Test
     public void testIsTrueString() {
-        fail("Not yet implemented");
+        assertFalse("null", BooleanUtil.isTrue((String)null));
+        assertFalse("empty", BooleanUtil.isTrue(""));
+        assertFalse("sjs", BooleanUtil.isTrue("sjs"));
+        assertFalse("space", BooleanUtil.isTrue(" "));
+        assertTrue("y", BooleanUtil.isTrue("y"));
+        assertTrue("yEs", BooleanUtil.isTrue("yEs"));
+        assertTrue("Y", BooleanUtil.isTrue("Y"));
+        assertTrue("YES", BooleanUtil.isTrue("YES"));
+        assertTrue("YeS", BooleanUtil.isTrue("YeS"));
+        assertTrue("t", BooleanUtil.isTrue("t"));
+        assertFalse("tru", BooleanUtil.isTrue("tru"));
+        assertTrue("true", BooleanUtil.isTrue("true"));
+        assertTrue("True", BooleanUtil.isTrue("True"));
+        assertTrue("t ", BooleanUtil.isTrue("t "));
     }
 
     @Test
     public void testIsFalse() {
-        fail("Not yet implemented");
+        assertFalse("null", BooleanUtil.isFalse((Boolean)null));
+        assertFalse("Boolean.TRUE", BooleanUtil.isFalse(Boolean.TRUE));
+        assertTrue("Boolean.FALSE", BooleanUtil.isFalse(Boolean.FALSE));
     }
-
 }
